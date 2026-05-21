f1382dd32d5aa2fe5b0e2a340a1d4814ec1f3b19
Louis301 <juri.orlik@yandex.ru>
2026-05-22 00:21:32 +0300

    env-set

---
+++ env-set.sh
@@ -0,0 +1,7 @@
+echo "# git_learning" >> README.md
+git init
+git add README.md
+git commit -m "env-set"
+git branch -M main
+git remote add origin https://github.com/Louis301/git_learning.git
+git push -u origin main
